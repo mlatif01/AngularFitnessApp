27a969874c5f7f2ad95402eaa627ac2fcce0548e
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-01-08 12:23:21 +0000

    add auth components

--- fitness-app/src/app/app.module.ts
+++ fitness-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
