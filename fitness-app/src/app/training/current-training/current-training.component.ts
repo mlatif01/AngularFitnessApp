09b29308c65f8a54436fc48ed46c6e7f342e4e83
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-01-09 12:40:20 +0000

    add stop training component

--- fitness-app/src/app/training/current-training/current-training.component.ts
+++ fitness-app/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -10,7 +12,7 @@ export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: number;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
     this.timer = setInterval( ()=> {
@@ -23,6 +25,7 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer);
+    this.dialog.open(StopTrainingComponent);
   }
 
 }
