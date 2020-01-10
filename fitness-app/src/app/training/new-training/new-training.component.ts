9ed37877fd870b4195e58735f181c1f28a33ae97
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-01-10 15:52:29 +0000

    add form to the training component

--- fitness-app/src/app/training/new-training/new-training.component.ts
+++ fitness-app/src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -8,8 +9,6 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-
-  @Output() trainingStart = new EventEmitter<void>();
 
   exercises: Exercise[];
 
@@ -19,8 +18,8 @@ export class NewTrainingComponent implements OnInit {
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining() {
-    this.trainingService.startExercise();
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 }
