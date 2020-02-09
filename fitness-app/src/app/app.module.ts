dbc44b7fcd75b86abb149e6782d95996f5b0a0fa
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-02-09 22:24:32 +0000

    split modules

--- fitness-app/src/app/app.module.ts
+++ fitness-app/src/app/app.module.ts
@@ -9,8 +9,6 @@ import { AngularFireAuthModule } from "@angular/fire/auth";
 
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material.module";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { LoginComponent } from "./auth/login/login.component";
 import { TrainingComponent } from "./training/training.component";
 import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
 import { NewTrainingComponent } from "./training/new-training/new-training.component";
@@ -24,12 +22,11 @@ import { AuthService } from "./auth/auth.service";
 import { TrainingService } from "./training/training.service";
 import { environment } from "../environments/environment";
 import { UIService } from "./shared/ui.service";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
     TrainingComponent,
     CurrentTrainingComponent,
     NewTrainingComponent,
@@ -49,7 +46,8 @@ import { UIService } from "./shared/ui.service";
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AuthModule
   ],
   providers: [AuthService, TrainingService, UIService],
   bootstrap: [AppComponent],
