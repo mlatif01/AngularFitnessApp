79081577ec5f82a519ecb9f7bdaa2d58bfd07191
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-02-10 05:19:58 +0000

    split training module

---
+++ fitness-app/src/app/training/training.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "../material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { TrainingComponent } from "../training/training.component";
+import { CurrentTrainingComponent } from "../training/current-training/current-training.component";
+import { NewTrainingComponent } from "../training/new-training/new-training.component";
+import { PastTrainingComponent } from "../training/past-training/past-training.component";
+import { StopTrainingComponent } from "../training/current-training/stop-training.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    StopTrainingComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  exports: [],
+  entryComponents: [StopTrainingComponent]
+})
+export class TrainingModule {}
